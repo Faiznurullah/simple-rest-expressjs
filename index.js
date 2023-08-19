e13af12959d757824a5fbaf926369cd474493194
unknown <faizn103a@gmail.com>
2023-08-19 11:09:35 +0700

    perbaikan struktur folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const siswaRoutes = require('./routes/siswa'); 
+const siswaRoutes = require('./src/routes/siswa'); 
 const bodyParser = require('body-parser');
 
 // Ubah path sesuai kebutuhan
